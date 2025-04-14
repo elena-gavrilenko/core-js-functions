e9fe34a92b3aaf81baff524f14f1bda2e99e31d5
Helena <gomel-offis@mail.ru>
2025-04-14 23:23:06 +0300

    feat: add task 6

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -119,8 +119,17 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cachedResult;
+  let isCalled = false;
+
+  return function () {
+    if (!isCalled) {
+      cachedResult = func();
+      isCalled = true;
+    }
+    return cachedResult;
+  };
 }
 
 /**
